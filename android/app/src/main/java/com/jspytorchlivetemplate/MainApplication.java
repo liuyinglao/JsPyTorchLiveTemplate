8387cabc429ae7eadacb8c5498991fdd0b2939cf
Yinglao Liu <yinglao@fb.com>
2022-05-13 15:53:20 -0700

    remove unecessary android package

--- android/app/src/main/java/com/jspytorchlivetemplate/MainApplication.java
+++ android/app/src/main/java/com/jspytorchlivetemplate/MainApplication.java
@@ -11,7 +11,6 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.facebook.react.bridge.JSIModulePackage;
-import org.pytorch.rn.core.PyTorchCorePackage;
 import org.pytorch.rn.core.jsi.PyTorchCoreJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
